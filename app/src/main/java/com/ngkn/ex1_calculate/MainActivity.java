debf62667b22c1ca11c1cf6ec53acc6a1a02d6b0
nhatnkse16 <nguyenkimnhat0110@gmail.com>
2024-05-12 15:30:30 +0700

    1

---
+++ app/src/main/java/com/ngkn/ex1_calculate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ngkn.ex1_calculate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculate_activity_main);
+    }
+}
